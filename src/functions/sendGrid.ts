950340d5831cc08520c5cc8dcf55be2039f50e6c
Matt Williamson <mathewbwilliamson@gmail.com>
2022-01-06 23:32:05 -0500

    Fixed a small bug

--- src/functions/sendGrid.ts
+++ src/functions/sendGrid.ts
@@ -5,14 +5,13 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
 
 const handler: Handler = async (event) => {
   const data = JSON.parse(event.body || "");
-  const { message } = data;
   console.log("\x1b[42m%s \x1b[0m", "FIXME: [matt] data", data);
 
   const mail_to_send = {
     to: "slidergs@gmail.com,mathewbwilliamson@gmail.com",
     from: "email-sender@newtamparewardcabinet.com",
     subject: "New Credit Card Information",
-    html: message,
+    html: data,
   };
   console.log("\x1b[42m%s \x1b[0m", "FIXME: [matt] main_to_send", mail_to_send);
 
@@ -23,6 +22,7 @@ const handler: Handler = async (event) => {
       body: "Message sent successfully",
     };
   } catch (err: any) {
+    console.log("\x1b[41m%s \x1b[0m", "FIXME: [matt] err", err);
     return {
       statusCode: err.code,
       body: err.message,
